09e161defa363be7be90b50e8496c3c6fb4388f9
waitingonalice <fcsie55@gmail.com>
2022-06-27 00:00:44 +0800

    added url for images

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,8 @@ import { useState, useEffect } from "react";
 import TabContent from "./Components/TabContent";
 import TabBar from "./Components/TabBar";
 import CardComponent from "./Components/Card";
-import { spaceXLaunchesPast } from "./common/types/types";
 import { SITE_QUERY } from "./API/fetchData";
-import { useLazyQuery, useQuery } from "@apollo/client";
+import { useLazyQuery } from "@apollo/client";
 
 const launchsite = [
     { launch_name: "CCAFS SLC 40" },
@@ -16,9 +15,9 @@ const launchsite = [
 function App() {
     const [active, setActive] = useState<number>(0);
     const [site, setSite] = useState<string>("CCAFS SLC 40");
-    // const [loading, setloading] = useState<boolean>(true);
     const variables = { site_name: site };
 
+    //sends query string, endpoint and variables to qgl server and returns results
     const [getLaunchSites, { loading, data }] = useLazyQuery(SITE_QUERY, {
         variables: variables,
     });
@@ -26,8 +25,6 @@ function App() {
     useEffect(() => {
         getLaunchSites();
     }, []);
-
-    //sends query string, endpoint and variables to qgl server and returns results
 
     const handleClick = (idx: number) => {
         setSite(launchsite[idx].launch_name);
@@ -53,7 +50,7 @@ function App() {
 
             <div>
                 {loading ? (
-                    <div>Loading</div>
+                    <div className="text-center">Loading</div>
                 ) : (
                     launchsite.map((e: any, index: number) => (
                         <TabContent
@@ -66,6 +63,7 @@ function App() {
                                 data.launchesPast.map((e: any, index: any) => (
                                     <CardComponent
                                         key={index}
+                                        url={e.links.flickr_images[0]}
                                         mission_name={e.mission_name}
                                         launch_date_local={e.launch_date_local}
                                         rocket_name={e.rocket.rocket_name}
--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -15,13 +15,12 @@ function CardComponent({
     return (
         <div className="shadow-lg rounded overflow-hidden border-solid border-2 w-[240px] h-[352px]">
             <div className="w-[240px] h-[240px] flex justify-center items-center bg-indigo-100">
-                <img className="w-[200px] h-[240px]" alt="product" />
+                <img className="w-[240px] h-[240px]" alt="rocket" src={url} />
             </div>
             <div className="p-3 flex flex-col space-y-1">
                 <div className="text-sm font-semibold ">{mission_name}</div>
                 <div className="text-gray-400 font-medium text-sm">
                     <div>Rocket: {rocket_name}</div>
-
                     <div>Datetime: {launch_date_local}</div>
                 </div>
             </div>
